c496387a4270851bb07053f22e2b23e3603168a0
Subin Shakya <subin12.shakya@gmail.com>
2024-12-27 23:25:04 +0545

    Add abort Controller

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("Could not fetch the data for that resource");
@@ -25,6 +27,8 @@ const useFetch = (url) => {
           setError(err.message);
         });
     }, 1000);
+
+    return () => abortCont.abort();
   }, [url]);
 
   return {
